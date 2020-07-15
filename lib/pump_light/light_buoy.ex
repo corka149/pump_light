c284e6ef47e46ee1b944a4b8fa96f65ac8aa3c9b
corka149 <corka149@mailbox.org>
2020-07-15 19:34:56 +0200

    Improved blink loop

--- lib/pump_light/light_buoy.ex
+++ lib/pump_light/light_buoy.ex
@@ -1,6 +1,7 @@
 defmodule PumpLight.LightBuoy do
 
   use Task, restart: :permanent
+  require Logger
 
   def start_link(_opts) do
     {:ok, socket} = :gen_udp.open(12347, [{:broadcast, true}])
@@ -8,14 +9,15 @@ defmodule PumpLight.LightBuoy do
   end
 
   def blink(socket) do
+    Logger.debug("Beating")
     :gen_udp.send(socket, {255, 255, 255, 255}, 12346, "Beat")
 
     receive do
       _anything ->
-        blink(socket)
+        Logger.debug("Received a message but did not handle it")
     after 60_000 ->
-      blink(socket)
+      Logger.debug("Waiting")
     end
+    blink(socket)
   end
-
 end
